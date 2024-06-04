db64668980aa892bd3ea475244a4a557c9e4bc88
wnccys <ipooo346@gmail.com>
2024-06-04 16:37:00 -0300

    feat: sets connection and send entrypoints

--- p2pClient/index.ts
+++ p2pClient/index.ts
@@ -2,7 +2,7 @@ import { randomUUID } from 'crypto';
 import * as net from 'net';
 import { EventEmitter } from 'events';
 
-module.exports = (options) => {
+module.exports = (options: any) => {
     // starts node
     return () => {
         // stops node
@@ -12,7 +12,27 @@ module.exports = (options) => {
 const connections = new Map;
 const emitter = new EventEmitter();
 
-const handleNewSocket = (socket) => {
+const send = (connectionId: String, message: String) => {
+    const socket = connections.get(connectionId);
+
+    if (!socket) {
+        throw new Error(`Attempt to send data to connection that does not exist ${connectionId}`);
+    }
+
+    socket.write(JSON.stringify(message));
+}
+
+// REVIEW: set correct types to params;
+const connect = (ip, port, cb) => {
+    const socket = new net.Socket();
+
+    socket.connect(port, ip, () => {
+        handleNewSocket(socket);
+        cb();
+    });
+}
+
+const handleNewSocket = (socket: net.Socket) => {
     const connectionId = randomUUID();
     connections.set(connectionId, socket);
 
@@ -21,7 +41,7 @@ const handleNewSocket = (socket) => {
         emitter.emit('disconnect', connectionId);
     });
 
-    socket.on('data', (data) => {
+    socket.on('data', (data: any) => {
         try {
             emitter.emit('message', { connectionId, message: JSON.parse(data.toString()) });
         } catch(err) {
@@ -30,6 +50,6 @@ const handleNewSocket = (socket) => {
     });
 }
 
-const server = net.createServer((socket) => {
+const server = net.createServer((socket: net.Socket) => {
     handleNewSocket(socket);
 });
