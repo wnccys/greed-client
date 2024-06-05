a1f6064d8bfff369dfe53a2d643d4bee8d028ec3
wnccys <ipooo346@gmail.com>
2024-06-05 19:13:31 -0300

    feat: sets full p2pClient

--- p2pClient/index.ts
+++ p2pClient/index.ts
@@ -16,25 +16,6 @@ type IpAdress = string;
 const connections = new Map;
 const emitter = new EventEmitter();
 
-const send = (connectionId: String, message: String) => {
-    const socket = connections.get(connectionId);
-
-    if (!socket) {
-        throw new Error(`Attempt to send data to connection that does not exist ${connectionId}`);
-    }
-
-    socket.write(JSON.stringify(message));
-}
-
-const connect = (ip: IpAdress, port: number, callback: Function) => {
-    const socket = new net.Socket();
-
-    socket.connect(port, ip, () => {
-        handleNewSocket(socket);
-        callback();
-    });
-}
-
 const handleNewSocket = (socket: net.Socket) => {
     const connectionId = randomUUID();
     connections.set(connectionId, socket);
@@ -53,14 +34,132 @@ const handleNewSocket = (socket: net.Socket) => {
     });
 }
 
+const send = (connectionId: String, message: Object) => {
+    const socket = connections.get(connectionId);
+
+    if (!socket) {
+        throw new Error(`Attempt to send data to connection that does not exist ${connectionId}`);
+    }
+
+    socket.write(JSON.stringify(message));
+}
+
+const connect = (ip: IpAdress, port: number, cb: Function) => {
+    const socket = new net.Socket();
+
+    socket.connect(port, ip, () => {
+        handleNewSocket(socket);
+        cb();
+    });
+}
+
 const server = net.createServer((socket: net.Socket) => {
     handleNewSocket(socket);
 });
 
 server.listen(options.port, 'localhost');
 
-return (cb: ((err?: Error | undefined) => void) | undefined) => {
-    server.close(cb);
+const NODE_ID = randomUUID();
+const neighbors = new Map();
+
+emitter.on('connect', (connectionId) => {
+    send(connectionId, { type: 'handshake', data: { nodeId: NODE_ID} } );
+});
+
+emitter.on('message', ({ connectionId, message }) => {
+    const { type, data } = message;
+
+    if (type === 'handshake') {
+        const { nodeId } = data;
+
+        neighbors.set(nodeId, connectionId);
+        emitter.emit('node-connect', { nodeId });
+    }
+
+    if (type === 'message') {
+        const nodeId = findNodeId(connectionId);
+
+        if (!nodeId) {
+            throw new Error("Couldn't determine connectionId");
+        }
+
+        emitter.emit('node-message', { nodeId, data });
+    }
+});
+
+const nodesend = (nodeId: String, data: Object) => {
+    const connectionId = neighbors.get(nodeId);
+
+    if (!connectionId) {
+        throw new Error("Couldn't determine connectionId");
+    }
+
+    send(connectionId, { type: 'message', data });
+};
+
+const findNodeId = (connectionId: String) => {
+    for (let [nodeId, $connectionId] of neighbors) {
+        if (connectionId = $connectionId) {
+            return nodeId;
+        }
+    }    
 }
+
+emitter.on('delete', (connectionId) => {
+    const nodeId = findNodeId(connectionId);
+
+    if (!nodeId) {
+        return console.error("error shut");
+    }
+
+    neighbors.delete(nodeId);
+    emitter.emit('node-disconnect', { nodeId });
+});
+
+const alreadySeenMessages = new Set();
+
+// REVIEW set correct message type/struct
+const p2psend = (data: any) => {
+    if (data.ttl < 1) {
+        return;
+    }
+
+    for (const $nodeId of neighbors.keys()) {
+        nodesend($nodeId, data);
+        alreadySeenMessages.add(data.id);
+    }
+};
+
+const broadcast = (message: Object, id = randomUUID(), origin = NODE_ID, ttl = 1000) => {
+    p2psend({ id, ttl, type: 'broadcast', message, origin });
+};
+
+const dm = (destination: string, message: Object, origin = NODE_ID, ttl = 10, id = randomUUID()) => {
+    p2psend({ id, ttl, type: 'dm', message, destination, origin });
+};
+
+emitter.on('node-message', ({ nodeId, data }) => {
+    if (!alreadySeenMessages.has(data.id)) {
+        if (data.type === 'broadcast'){
+            emitter.emit('broadcast', { message: data.message, origin: data.origin });
+            broadcast(data.message, data.id, data.origin, data.ttl - 1);
+        }
+
+        if (data.type === 'dm') {
+            if (data.destination === NODE_ID) {
+                emitter.emit('dm', { origin: data.origin, message: data.message });
+            } else {
+                dm(data.destination, data.message, data.origin, data.ttl - 1, data.id);
+            }
+        }
+    }
+});
+
+return {
+    broadcast, dm, on: emitter.on, connect,
+    close: () => {
+        server.close();
+    },
+};
 
 };
