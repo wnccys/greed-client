5677c0cac9fdd7338d21bccbe291cd0d5fe7aa2f
wnccys <ipooo346@gmail.com>
2024-06-13 14:03:43 -0300

    feat: parse-torrent test

--- bittorrentEx/main.ts
+++ bittorrentEx/main.ts
@@ -1,24 +1,8 @@
 import fs from 'fs';
-import { Bencode } from 'bencode-ts';
+// import { Bencode } from 'bencode-ts';
+import parseTorrent from 'parse-torrent';
 import * as dgram from 'dgram';
-import * as urlLib from 'url';
+import * as url from 'url';
 
-const torrent = Bencode.encode(fs.readFileSync('./puppy.torrent'));
-// FIXME verify port integrity
-const url = urlLib.parse(torrent.toString('utf-8'));
-console.log(url);
-const socket = dgram.createSocket('udp4');
-const myMsg = Buffer.from('hello?', 'utf-8');
-
-// sends message(handshake string 'hello') 
-// as a buffer from the previously url infos;
-socket.send(myMsg,
-    0, myMsg.length,
-    Number(url.port),
-    url.host || undefined, () => {});
-
-socket.on('message', msg => {
-    console.log("message from tracker: ", msg);
-});
-
-console.log(torrent.toString('utf-8'));
+const parsedTorrent = parseTorrent(fs.readFileSync('./puppy.torrent'));
+console.log(parsedTorrent);
