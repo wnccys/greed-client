753712c27ade5d1c7f1a45a9fdf5332a9b98c74b
wnccys <ipooo346@gmail.com>
2024-06-15 19:32:28 -0300

    ref: removes renamed folder

--- bittorrentEx/main.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import { Bencode } from 'bencode-ts';
-import * as dgram from 'dgram';
-import * as url from 'url';
-
-const torrent = Bencode.encode(fs.readFileSync('./puppy.torrent'));
-const urlParsed = url.parse(torrent.toString('utf8'));
-console.log(urlParsed);
