be6792882683edbab1b1b3571e406915cad9f39e
wnccys <ipooo346@gmail.com>
2024-06-15 18:27:06 -0300

    feat: sets basic UDP request

--- bittorrent/main.ts
+++ bittorrent/main.ts
@@ -5,4 +5,13 @@ import * as url from 'url';
 
 const torrent = Bencode.decode(fs.readFileSync('./tears-of-steel.torrent'), 'utf-8');
 const urlParsed = url.parse(torrent.announce.toString('utf8'));
-console.log(urlParsed);
+console.log(urlParsed.host);
+
+const socket = dgram.createSocket('udp4');
+const myMsg = Buffer.from('hello?', 'utf-8');
+
+socket.send(myMsg, 0, myMsg.length, Number(urlParsed.port), urlParsed.hostname || undefined, (some) => {console.log(some)})
+
+socket.on('message', (msg) => {
+    console.log('message is: ', msg);
+});
