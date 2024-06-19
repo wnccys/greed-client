de20f2371d1f3b3610efee42f3558f51a2dd1529
wnccys <ipooo346@gmail.com>
2024-06-19 16:04:27 -0300

    feat: sets misc functions

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { getPeers } from '@tracker/tracker';
+import { Torrent } from '@classes/torrent';
 import * as TorrentParser from './torrentParser'
-import { newTorrent } from '@customTypes/torrent'
 import 'tsconfig-paths/register';
 
 const torrentFile = './src/torrents/tears-of-steel.torrent';
-// REVIEW set Torrent type as class;
-const torrent = newTorrent(TorrentParser.open(torrentFile));
+const torrent = new Torrent(TorrentParser.open(torrentFile));
 
 getPeers(torrent, (peers: any) => {
     console.log('list of peers: ', peers);
--- src/torrentParser.ts
+++ src/torrentParser.ts
@@ -1,5 +1,18 @@
-import { Torrent } from '@customTypes/torrent';
+import { Bencode } from 'bencode-ts';
+import { Torrent } from '@classes/torrent';
+import fs from 'fs';
+import crypto from 'crypto';
 
-export function infoHash(torrent: Torrent) {
+export function open(filepath: string): any {
+    return Bencode.decode(fs.readFileSync(filepath));
+}
 
+export function size(torrent: Torrent): any {
+
+}
+
+export function infoHash(torrent: Torrent): Buffer {
+// NOTE - possibly use console-ninja to debug torrent.info
+    const info = Bencode.encode(torrent.info);
+    return crypto.createHash('sha1').update(info).digest();
 }
