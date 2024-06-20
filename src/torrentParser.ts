762edf6f13f1c69f8e0d680f7a5f358f316e4a2a
wnccys <ipooo346@gmail.com>
2024-06-20 14:31:53 -0300

    feat: adds torrentInfo type and size function to torrrentParser

--- src/torrentParser.ts
+++ src/torrentParser.ts
@@ -7,12 +7,20 @@ export function open(filepath: string): any {
     return Bencode.decode(fs.readFileSync(filepath));
 }
 
-export function size(torrent: Torrent): any {
+export function size(torrent: Torrent): Buffer {
+    // REVIEW - verify if it needs decoded pieces;
+    const size = torrent.info.files ? 
+        torrent.info.files.map(file => file.length).reduce((a, b) => a+b) :
+        torrent.info['piece-length'];
 
+    let buf = Buffer.alloc(8); 
+    buf.writeBigInt64BE(size, 0);
+    // REVIEW check for BigInt require;
+    return buf;
 }
 
 export function infoHash(torrent: Torrent): Buffer {
-// NOTE - possibly use console-ninja to debug torrent.info
+    // NOTE - possibly use console-ninja to debug torrent.info;
     const info = Bencode.encode(torrent.info);
     return crypto.createHash('sha1').update(info).digest();
 }
