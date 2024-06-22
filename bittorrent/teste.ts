df36fd34532b20293754a5f31a042d3232669c60
Patricio0022 <niellucsx@hotmail.com>
2024-06-22 19:43:41 -0300

    att

--- bittorrent/teste.ts
+++ bittorrent/teste.ts
@@ -1,9 +1,9 @@
 import fs from 'fs';
-import { Bencode } from 'bencode-ts';
+import * as bencode from 'bencode-ts';
 import * as dgram from 'dgram';
 import * as url from 'url';
 
-const torrent = Bencode.decode(fs.readFileSync('./tears-of-steel.torrent'), 'utf-8');
+const torrent = bencode.decode(fs.readFileSync('./tears-of-steel.torrent'), 'utf-8');
 const urlParsed = url.parse(torrent.announce.toString('utf-8'));
 console.log(urlParsed.href);
 console.log(torrent);
