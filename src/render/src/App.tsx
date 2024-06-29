f4ad199e7276486163d1f7a52f36511a1a75c43f
wnccys <ipooo346@gmail.com>
2024-06-28 22:35:58 -0300

    feat: adds simple render structure

--- src/render/src/App.tsx
+++ src/render/src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+import LandscapeIcon from '@mui/icons-material/Landscape';
+import FileOpenIcon from '@mui/icons-material/FileOpen';
+import { AppBar, Button, Box, Container, Typography, Toolbar, IconButton, CssBaseline } from '@mui/material';
 
 export function App() {
+  async function initTorrentDownload() {
+    //TODO
+  }
   return (
-    <Button variant="contained">Hello Torrent</Button>
+    <>
+      <Box sx={{
+        flexGrow: 1,
+        whiteSpace: 'nowrap',
+        button: {
+          color: 'inherit',
+        },
+      }}>
+      </Box>
+    </>
   )
 }
