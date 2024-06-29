8507b674e32844a9ba3b795255aa01b16d99f8c2
wnccys <ipooo346@gmail.com>
2024-06-28 21:59:23 -0300

    feat: sets hello basic button

--- src/render/src/App.tsx
+++ src/render/src/App.tsx
@@ -1,11 +1,8 @@
+import React from 'react';
 import Button from '@mui/material/Button';
-import LandscapeIcon from '@mui/icons-material/Landscape';
-import fileOpenIcon from '@mui/icons-material/FileOpen';
-import { AppBar, Box, Container, CssBaseline, IconButton, Toolbar, Typography } from '@mui/material';
-import ImageFilter from 'react-image-filter';
-import { useState } from 'react';
 
-function App() {
-  const [imageFilter, setImageFilter] = useState(undefined);
-  const [imageUrl, setImageUrl] = useState('https://source.unsplash.com/RZrIJ8C0860');
+export function App() {
+  return (
+    <Button variant="contained">Hello Torrent</Button>
+  )
 }
