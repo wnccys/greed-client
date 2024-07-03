31bb71c2d45fc8d28458f66d025e032a429d63cc
wnccys <ipooo346@gmail.com>
2024-07-03 16:34:01 -0300

    feat: adds shadcn and changes background

--- src/render/src/App.tsx
+++ src/render/src/App.tsx
@@ -1,3 +1,4 @@
+import { Box, Container } from '@mui/material'
 import { DownloadButton } from './components/DownloadButton'
 
 export function App() {
@@ -7,7 +8,14 @@ export function App() {
 
   return (
     <>
-      <DownloadButton/>
+      <Container sx={{
+        padding: "5em",
+      }}>
+        <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+          This Box renders as an HTML section element.
+        </Box>
+        <DownloadButton />
+      </Container>
     </>
   )
 }
--- src/render/src/components/DownloadButton.tsx
+++ src/render/src/components/DownloadButton.tsx
@@ -1,10 +1,9 @@
-import * as React from 'react';
 import Button from '@mui/material/Button';
 
 export function DownloadButton() {
-    return (
-			<Button variant="outlined">
-				Hello World!
-			</Button>
-    )
+	return (
+		<Button variant="outlined">
+			Hello World!
+		</Button>
+	)
 }
