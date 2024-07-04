d72af48f254aa51fe8fcdd10003e3be91893f1c5
wnccys <ipooo346@gmail.com>
2024-07-04 15:42:27 -0300

    feat: sets basic page

--- src/render/src/App.tsx
+++ src/render/src/App.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button"
 
 export function App() {
   return (
-    <div className='p-6 max-w-4xl mx-auto'>
-      <Button>Butao</Button>
-      <InputFile />
+      <div className='p-6 max-w-4xl mx-auto border border-zinc-800'>
+        <InputFile />
+        <Button className ="mt-5">Download Torrent</Button>
     </div>
   )
 }
