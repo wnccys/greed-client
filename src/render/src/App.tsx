7e401aec8e104499cc260263525fcefd949ff1e8
wnccys <ipooo346@gmail.com>
2024-07-04 15:31:56 -0300

    feat: adds shadcn button

--- src/render/src/App.tsx
+++ src/render/src/App.tsx
@@ -1,8 +1,10 @@
-import { InputFile } from "./components/ui/inputFile";
+import { InputFile } from "@/components/ui/inputFile";
+import { Button } from "@/components/ui/button"
 
 export function App() {
   return (
     <div className='p-6 max-w-4xl mx-auto'>
+      <Button>Butao</Button>
       <InputFile />
     </div>
   )
