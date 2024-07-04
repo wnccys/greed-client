c7a6b140b1ae76c36d1be14708aecd8cdeb18887
wnccys <ipooo346@gmail.com>
2024-07-03 22:54:08 -0300

    ref: removes unused file

--- src/render/src/theme.ts
+++
@@ -1,9 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-    typography: {
-        fontFamily: 'Roboto, Arial, sans-serif'
-    }
-});
-
-export default theme;
