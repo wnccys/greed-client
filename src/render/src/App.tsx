278722e7d29badcd2fca6ce8dd26503f66a2d6ec
wnccys <ipooo346@gmail.com>
2024-07-08 15:44:07 -0300

    feat: adds basic webotorrent embed test

--- src/render/src/App.tsx
+++ src/render/src/App.tsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import { InputFile } from "@/components/ui/inputFile";
 import { Button } from "@/components/ui/button";
-import { CardDemo } from "@/components/ui/CardContent";
+import { CardDemo } from "@/components/ui/cardContent";
+import fs from 'fs';
+import WebTorrent, { Torrent } from "webtorrent";
+// import { ChangeEvent } from "react";
 
 export function App() {
+  const [torrentFile, setTorrentFile] = useState('');
+
+  function handleDownloadClick() {
+    const dummyFile = fs.readFileSync('../../torrent_files/small.torrent', 'utf-8');
+    if (dummyFile === '') return
+
+    setTorrentFile(dummyFile);
+    const client = new WebTorrent()
+    client.add(torrentFile, (torrent: Torrent) => {
+      console.log(torrent);
+    });
+  }
+
+  // const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   if (e.target.files[0]) {
+  //     setTorrentFile(e.target.files[0]);
+  //   }
+  // };
+
   return (
     <div className='mt-14 p-6 mx-auto border border-zinc-800'>
       <CardDemo className="mb-5" />
-      <InputFile />
-      <Button className ="mt-5">Download Torrent</Button>
+      {/* <InputFile onChange={handleFileChange}/> */}
+      <InputFile /> 
+      <Button className ="mt-5" onClick={handleDownloadClick}>Download Torrent</Button>
   </div>
   )
 }
--- src/render/src/components/ui/inputFile.tsx
+++ src/render/src/components/ui/inputFile.tsx
@@ -1,9 +1,12 @@
 import { Input } from "@/components/ui/input"
+interface inputFileProps {
+  onChange?: () => void,
+}
 
-export function InputFile() {
+export function InputFile({ onChange }: inputFileProps) {
   return (
     <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Input id="picture" type="file" />
+      <Input id="picture" onClick={onChange} type="file" />
     </div>
   )
 }
