cde3a476588318d41f94c2da9a69192d1e869e16
wnccys <ipooo346@gmail.com>
2024-07-08 16:20:00 -0300

    feat: sets node-polyfills and optimizes webtorrent

--- src/render/vite.config.ts
+++ src/render/vite.config.ts
@@ -1,9 +1,13 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [nodePolyfills(), react()],
+  optimizeDeps: {
+    include: ['webtorrent/webtorrent.min.js'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
