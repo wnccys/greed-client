6c005245dc3690abb1cf26cc4965e021b6f2d2e5
wnccys <ipooo346@gmail.com>
2024-07-09 16:33:47 -0300

    feat: sets basic axios request to localhost

--- src/render/src/App.tsx
+++ src/render/src/App.tsx
@@ -1,26 +1,31 @@
+import axios, { AxiosResponse } from 'axios';
 import { ChangeEvent, useState } from 'react';
 import { InputFile } from "@/components/ui/inputFile";
 import { Button } from "@/components/ui/button";
 import { CardDemo } from "@/components/ui/cardContent";
-import WebTorrent, { Torrent } from "webtorrent";
+
+const baseURL = 'http://10.0.0.41/5172';
 
 export function App() {
   const [torrentFile, setTorrentFile] = useState<File>();
+  const [response, setResponse] = useState<AxiosResponse>();
 
-  function handleDownloadClick() {
-    if (!torrentFile) return;
-
-    const client = new WebTorrent()
-    client.add(torrentFile, (torrent: Torrent) => {
-      console.log("Downloading Torrent: ", torrent);
+  function downloadTorrent() {
+    console.log("Torrent File: ", torrentFile?.name);
+    axios.post(baseURL, {
+        body: torrentFile,
+      })
+    .then((axiosResponse) => {
+      setResponse(axiosResponse);
+      console.log(response);
     });
+
+    console.log("Response from server: ", response);
   }
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       setTorrentFile(e.target.files[0]);
-    } else {
-      throw Error("Could not open selected file.");
     }
   }
 
@@ -28,7 +33,7 @@ export function App() {
     <div className='mt-14 p-6 mx-auto border border-zinc-800'>
       <CardDemo className="mb-5" />
       <InputFile onChange={handleFileChange}/>
-      <Button className ="mt-5" onClick={handleDownloadClick}>Download Torrent</Button>
+      <Button className ="mt-5" onClick={downloadTorrent}>Download Torrent</Button>
   </div>
   )
 }
