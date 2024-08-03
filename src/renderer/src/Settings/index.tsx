29fbbbeee14b059a4c6bc449b3cbf9f2467cbc3f
wnccys <ipooo346@gmail.com>
2024-08-02 23:17:12 -0300

    feat: sets basic source table

--- src/renderer/src/Settings/index.tsx
+++ src/renderer/src/Settings/index.tsx
@@ -1,5 +1,39 @@
+import { Button } from "@renderer/components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@renderer/components/ui/table"
+
 export function Settings() {
     return (
-        "Hello Settings!"
+        <div className="flex flex-col items-center mt-5 p-5">
+            <p className="text-xl">Current Torrent Sources</p>
+            <div>
+                <Table className="mt-5">
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[160px]">Source Name</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead className="text-right">Repacks</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell className="font-medium">FitGirl Repacks</TableCell>
+                            <TableCell>Syncronized</TableCell>
+                            <TableCell className="text-right">1000</TableCell>
+                            <TableCell className="text-right">
+                                <Button className="bg-zinc-800 hover:bg-red-500">Remove</Button>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+        </div>
     )
 }
