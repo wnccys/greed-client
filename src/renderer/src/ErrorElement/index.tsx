899adbe70eb6b625543a9eacca6a0979414a3904
wnccys <ipooo346@gmail.com>
2024-08-02 22:34:52 -0300

    feat: sets correct 404 not found style

--- src/renderer/src/ErrorElement/index.tsx
+++ src/renderer/src/ErrorElement/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function ErrorElement () {
     return (
-        <div className="align-middle">
+        <div className="flex flex-col justify-center items-center bg-[#171717] h-[100vh] w-[100vw]">
             <h1>404 NOT FOUND</h1>
             <Link to="/">
                Back to Application 
