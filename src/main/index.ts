aa89df291eb5c07789b241879686a8cd1660a0d6
Patricio0022 <niellucsx@hotmail.com>
2024-08-03 23:11:09 -0300

    add

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,154 +5,108 @@ import {
 	dialog,
 	Menu,
 	type IpcMainInvokeEvent,
-} from "electron";
-import { optimizer } from "@electron-toolkit/utils";
-import path from "node:path";
-import { initTorrentDownload } from "./torrentClient";
-
-async function handleFileOpen(): Promise<Array<string>> {
+  } from "electron";
+  import { optimizer } from "@electron-toolkit/utils";
+  import path from "node:path";
+  import { initTorrentDownload } from "./torrentClient";
+  
+  async function handleFileOpen(): Promise<Array<string>> {
 	const { canceled, filePaths } = await dialog.showOpenDialog({
-		title: "Select File",
-		properties: ["openFile"]
+	  title: "Select File",
+	  properties: ["openFile"]
 	});
-
+  
 	if (!canceled) {
-		console.log(filePaths);
-		return [`Selected File: ${path.basename(filePaths[0])}`, filePaths[0]];
+	  console.log(filePaths);
+	  return [`Selected File: ${path.basename(filePaths[0])}`, filePaths[0]];
 	}
-
+  
 	return ["", "Please, Select a Valid Torrent File"];
-}
-
-async function handleTorrentPath(_event: IpcMainInvokeEvent, path: string) {
+  }
+  
+  async function handleTorrentPath(_event: IpcMainInvokeEvent, path: string) {
 	console.log("path to torrent is: ", path);
 	const { canceled, filePaths } = await dialog.showOpenDialog({
-		title: "Select Folder",
-		properties: ["openDirectory", "createDirectory"],
+	  title: "Select Folder",
+	  properties: ["openDirectory", "createDirectory"],
 	});
-
+  
 	if (!canceled) {
-		initTorrentDownload(path, filePaths[0]);
+	  initTorrentDownload(path, filePaths[0]);
 	}
-}
-
-const createWindow = () => {
+  }
+  
+  const createWindow = () => {
 	const mainWindow = new BrowserWindow({
-		width: 900,
-		height: 670,
-		autoHideMenuBar: true,
-		webPreferences: {
-			preload: path.join(__dirname, "../preload/index.mjs"),
-			sandbox: false,
-			webSecurity: false,
-		},
-		titleBarStyle: 'hidden',
-		titleBarOverlay: {
-			color: '#171717',
-			symbolColor: '#F5F5F5',
-			height: 25,
-		}
+	  width: 900,
+	  height: 670,
+	  autoHideMenuBar: true,
+	  webPreferences: {
+		preload: path.join(__dirname, "../preload/index.mjs"),
+		sandbox: false,
+		webSecurity: false,
+	  },
+	  titleBarStyle: 'hidden',
+	  titleBarOverlay: {
+		color: '#171717',
+		symbolColor: '#F5F5F5',
+		height: 25,
+	  }
 	});
-
+  
 	Menu.setApplicationMenu(null);
-
+  
 	mainWindow.loadURL("http://localhost:5173");
-
+  
 	mainWindow.on("ready-to-show", () => {
-		mainWindow.show();
+	  mainWindow.show();
 	});
-};
-
-app.whenReady().then(() => {
+  };
+  
+  const createSecond = () => {
+	const secondaryWindow = new BrowserWindow({
+	  width: 900,
+	  height: 670,
+	  autoHideMenuBar: true,
+	  webPreferences: {
+		preload: path.join(__dirname, "../preload/index.mjs"),
+		sandbox: false,
+		webSecurity: false,
+	  },
+	  titleBarStyle: 'hidden',
+	  titleBarOverlay: {
+		color: '#171717',
+		symbolColor: '#F5F5F5',
+		height: 25,
+	  }
+	});
+  
+	Menu.setApplicationMenu(null);
+  
+	secondaryWindow.loadURL("http://localhost:5173/download");
+  
+	secondaryWindow.on("ready-to-show", () => {
+	  secondaryWindow.show();
+	});
+  };
+  
+  app.whenReady().then(() => {
 	ipcMain.handle("handleFileSelect", handleFileOpen);
 	ipcMain.handle("sendTorrentPath", handleTorrentPath);
-
-	app.on("activate", () => {
-		if (BrowserWindow.getAllWindows().length === 0) createWindow();
-	});
-
+  
 	createWindow();
-});
-
-app.on("window-all-closed", () => {
+	createSecond();
+  
+	app.on("activate", () => {
+	  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+  });
+  
+  app.on("window-all-closed", () => {
 	if (process.platform !== "darwin") app.quit();
-});
-
-app.on("browser-window-created", (_, window) => {
+  });
+  
+  app.on("browser-window-created", (_, window) => {
 	optimizer.watchWindowShortcuts(window);
-});
-
-// import { app, shell, BrowserWindow, ipcMain } from 'electron'
-// import { join } from 'path'
-// import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-// import icon from '../../resources/icon.png?asset'
-
-// function createWindow(): void {
-//   // Create the browser window.
-//   const mainWindow = new BrowserWindow({
-//     width: 900,
-//     height: 670,
-//     show: false,
-//     autoHideMenuBar: true,
-//     ...(process.platform === 'linux' ? { icon } : {}),
-//     webPreferences: {
-//       preload: join(__dirname, '../preload/index.js'),
-//       sandbox: false
-//     }
-//   })
-
-// mainWindow.on('ready-to-show', () => {
-//   mainWindow.show()
-// })
-
-// mainWindow.webContents.setWindowOpenHandler((details) => {
-//   shell.openExternal(details.url)
-//   return { action: 'deny' }
-// })
-
-//   // HMR for renderer base on electron-vite cli.
-//   // Load the remote URL for development or the local html file for production.
-//   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-//     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
-//   } else {
-//     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
-//   }
-// }
-
-// // This method will be called when Electron has finished
-// // initialization and is ready to create browser windows.
-// // Some APIs can only be used after this event occurs.
-// app.whenReady().then(() => {
-//   // Set app user model id for windows
-//   electronApp.setAppUserModelId('com.electron')
-
-//   // Default open or close DevTools by F12 in development
-//   // and ignore CommandOrControl + R in production.
-//   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-//   app.on('browser-window-created', (_, window) => {
-//     optimizer.watchWindowShortcuts(window)
-//   })
-
-//   // IPC test
-//   ipcMain.on('ping', () => console.log('pong'))
-
-//   createWindow()
-
-//   app.on('activate', function () {
-//     // On macOS it's common to re-create a window in the app when the
-//     // dock icon is clicked and there are no other windows open.
-//     if (BrowserWindow.getAllWindows().length === 0) createWindow()
-//   })
-// })
-
-// // Quit when all windows are closed, except on macOS. There, it's common
-// // for applications and their menu bar to stay active until the user quits
-// // explicitly with Cmd + Q.
-// app.on('window-all-closed', () => {
-//   if (process.platform !== 'darwin') {
-//     app.quit()
-//   }
-// })
-
-// // In this file you can include the rest of your app"s specific main process
-// // code. You can also put them in separate files and require them here.
+  });
+  
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,9 +4,11 @@ import './assets/global.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { Download } from './Download'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Download/>
   </React.StrictMode>
 )
