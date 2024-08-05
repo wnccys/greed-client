1b311f6c0a6a168ab5c8721802b9f765954c08b6
wnccys <ipooo346@gmail.com>
2024-08-05 16:24:48 -0300

    feat: sets corrects typeORM base config and sqlite3 connection

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,10 +1,10 @@
 import { resolve } from 'node:path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin, swcPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin(), swcPlugin()]
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
@@ -13,6 +13,8 @@ export default defineConfig({
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src'),
+        '@main': resolve('src/main'),
+        '@preload': resolve('src/preload'),
       }
     },
     plugins: [react()]
--- src/main/data-source.ts
+++ src/main/data-source.ts
@@ -1,9 +1,10 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User.js"
+import { User } from "@main/entity/User"
 
 export const AppDataSource = new DataSource({
-    
+    type: "sqlite",
+    database: "database.sqlite",
     synchronize: true,
     logging: false,
     entities: [User],
