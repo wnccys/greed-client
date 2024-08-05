0eedd1305fbe1882ca61e9394db740eb5eea6291
wnccys <ipooo346@gmail.com>
2024-08-05 14:54:03 -0300

    feat: sets correct style for dialog and table hover

--- src/renderer/src/Settings/index.tsx
+++ src/renderer/src/Settings/index.tsx
@@ -21,6 +21,7 @@ import {
 	DialogTrigger,
 } from "@renderer/components/ui/dialog";
 import { useRef, useState } from "react";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
 export function Settings() {
 	const sourceNameRef = useRef<HTMLInputElement>(null);
@@ -63,7 +64,7 @@ export function Settings() {
 			<p className="text-xl">Current Torrent Sources</p>
 			<div>
 				<Table className="mt-5">
-					<TableHeader>
+					<TableHeader className="hover:bg-zinc-900">
 						<TableRow>
 							<TableHead className="w-[160px]">Source Name</TableHead>
 							<TableHead>Status</TableHead>
@@ -71,7 +72,7 @@ export function Settings() {
 						</TableRow>
 					</TableHeader>
 					<TableBody>
-						<TableRow>
+						<TableRow className="hover:bg-zinc-700">
 							<TableCell className="font-medium">FitGirl Repacks</TableCell>
 							<TableCell>Syncronized</TableCell>
 							<TableCell className="text-right">1000</TableCell>
@@ -106,6 +107,15 @@ export function Settings() {
 								</a>
 								.
 							</DialogDescription>
+							<div
+								className="absolute right-4 top-2 rounded-sm opacity-70 
+									ring-offset-background 
+									transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 
+									focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none 
+									data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+							>
+								<Cross2Icon className="h-4 w-4 cursor-pointer" onClick={() => setIsDialogOpen(false)}	/>
+							</div>
 						</DialogHeader>
 						<div className="grid gap-4 py-4">
 							<div className="grid grid-cols-4 items-center gap-4">
