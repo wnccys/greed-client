912cad27d98d115f0af568dcc2e46340b76fa8b8
wnccys <ipooo346@gmail.com>
2024-08-06 23:09:36 -0300

    feat: sets correct play-menu (mt foda)

--- src/renderer/src/SelectedGame/index.tsx
+++ src/renderer/src/SelectedGame/index.tsx
@@ -3,6 +3,7 @@ import GameDummyImage from "@renderer/assets/image.png";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { DoubleArrowLeftIcon } from "@radix-ui/react-icons";
+import { Button } from "@renderer/components/ui/button";
 
 export function SelectedGame() {
 	const [downloadResult, setDownloadResult] = useState<string | undefined>(
@@ -21,12 +22,26 @@ export function SelectedGame() {
 				<img src={GameDummyImage} alt="game-cover" className="" />
 			</div>
 
-			<div id="play-menu" className="flex justify-center pt-5">
+			<div id="play-menu" 
+				className="flex justify-center pt-5 transition delay-150
+				drop-shadow-lg shadow-black duration-[300]">
 				<div
-					className="absolute transform -translate-y-2/3 bg-[#242424] 
-                        rounded-3xl text-white p-4"
+					className="absolute transform -translate-y-3/4 bg-[#242424] 
+                        rounded-xl text-white w-[25em] flex justify-center"
 				>
-					<InputFile updateDownloadResult={setDownloadResult} />
+					<div className="p-3 w-full">
+						<h1>Baldur's Gate III</h1>
+						<p className="text-xs text-zinc-700 font-bold mt-[1em]">Play Time: 1540hrs</p>
+					</div>
+					<div className="h-full self-center p-0">
+						<Button 
+						className="p-6 bg-white text-zinc-900 hover:text-white w-full 
+						h-full ps-10 pe-10 text-lg transition delay-150 duration-300"
+					>
+							Run
+						</Button>
+					</div>
+					{/* <InputFile updateDownloadResult={setDownloadResult} /> */}
 					{/* <p className={`pt-4 ${downloadResult ? '' : 'hidden p-0 m-0'}`}>{downloadResult}</p> */}
 				</div>
 			</div>
