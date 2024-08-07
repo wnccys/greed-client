470728cab6dade7dc60070dd4d8ce86130d4a87f
wnccys <ipooo346@gmail.com>
2024-08-06 22:23:52 -0300

    feat: sets missing link to selected route

--- src/renderer/src/components/ui/GameCard.tsx
+++ src/renderer/src/components/ui/GameCard.tsx
@@ -7,30 +7,32 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@renderer/components/ui/card";
+import { Link } from "react-router-dom";
 
 export function GameCards() {
     return (
         <>
             {Array.from({ length: 6 }).map((_, index) => {
                 return (
-                    <Card
-                        // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-                        key={index}
-                        className="border shadow-lg 
-                        hover:drop-shadow-2xl shadow-black rounded-md h-[10em] 
-                        w-[22em] mt-8 cursor-pointer overflow-hidden transition 
-                        delay-150 hover:-translate-y-1 hover:scale-105 duration-[300]"
-                    >
-                        <CardContent className="p-0">
-                            <img
-                                src={GameDummyImage}
-                                alt="game-cover"
-                                className="rounded-lg"
-                            />
-                            <p className="p-2 absolute -translate-y-12 z-10">Baldur's Gate III</p>
-                            <p className="text-xs">cuzao</p>
-                        </CardContent>
-                    </Card>
+                    // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                    <Link to="../selected-game" key={index}>
+                        <Card
+                            className="border shadow-lg 
+                            hover:drop-shadow-2xl shadow-black rounded-md h-[10em] 
+                            w-[22em] mt-8 cursor-pointer overflow-hidden transition 
+                            delay-150 hover:-translate-y-1 hover:scale-105 duration-[300]"
+                        >
+                            <CardContent className="p-0">
+                                <img
+                                    src={GameDummyImage}
+                                    alt="game-cover"
+                                    className="rounded-lg"
+                                />
+                                <p className="p-2 absolute -translate-y-12 z-10">Baldur's Gate III</p>
+                                <p className="text-xs">cuzao</p>
+                            </CardContent>
+                        </Card>
+                    </Link>
                 )
             })}
         </>
