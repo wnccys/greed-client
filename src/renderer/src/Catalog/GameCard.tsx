041fdf8646710abdb6ca355b1edbf78c66a50785
wnccys <ipooo346@gmail.com>
2024-08-08 10:44:04 -0300

    fix: another approach to card sizes

--- src/renderer/src/Catalog/GameCard.tsx
+++ src/renderer/src/Catalog/GameCard.tsx
@@ -16,6 +16,7 @@ export function GameCard() {
                     <Card
                         className="border shadow-lg 
                         hover:drop-shadow-2xl shadow-black rounded-md h-[15.5vh] 
+                        lg:w-60 lg:h-36 md:w-52 md:h-24
                         w-[20vw] mt-8 cursor-pointer transition delay-15
                         hover:-translate-y-1 hover:scale-105 duration-[300]"
                     >
