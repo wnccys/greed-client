c3108162946310413b44c981d28f594e26530697
wnccys <ipooo346@gmail.com>
2024-08-14 21:38:56 -0300

    chore: renames assets to Assets

---
+++ src/renderer/src/Assets/icons.ts
@@ -0,0 +1,15 @@
+import CatalogIcon from './icons/icon-catalog.svg';
+import LibraryIcon from './icons/icon-library.svg';
+import DownloadIcon from './icons/icon-downloads.svg';
+import SettingsIcon from './icons/icon-settings.svg';
+import GreedIcon from './icons/icon-greed.svg'
+import SearchIcon from './icons/icon-search.svg'
+
+export {
+  CatalogIcon,
+  LibraryIcon,
+  DownloadIcon,
+  SettingsIcon,
+  GreedIcon,
+  SearchIcon
+};
