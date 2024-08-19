374e3fb2bc09decc8c53510476231b714acf223c
wnccys <ipooo346@gmail.com>
2024-08-19 16:37:02 -0300

    feat: sets basic configuration tabs

--- src/renderer/src/Settings/index.tsx
+++ src/renderer/src/Settings/index.tsx
@@ -93,219 +93,173 @@ export function Settings() {
 	return (
 		<div
 			className="flex flex-col items-center self-center mt-[10em] p-5 
-	cursor-default rounded h-screen"
+			cursor-default rounded h-screen"
 		>
 			<Tabs.Root
-				className="flex flex-col w-[300px] shadow-[0_2px_10px] shadow-blackA2"
+				className="flex flex-col shadow-lg shadow-black w-[30rem]"
 				defaultValue="tab1"
 			>
 				<Tabs.List className="shrink-0 flex border-b border-mauve6" aria-label="Manage your account">
 				<Tabs.Trigger
-					className="bg-zinc-900 px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
+					className="bg-zinc-900 px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] 
+					leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md 
+					hover:text-violet11 data-[state=active]:text-violet11 
+					data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current 
+					data-[state=active]:focus:relative 
+					outline-none cursor-default"
 					value="tab1"
 				>
-					Account
+					General Settings
 				</Tabs.Trigger>
 				<Tabs.Trigger
-					className="bg-zinc-900 px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
+					className="bg-zinc-900 px-5 h-[45px] flex-1 flex items-center justify-center 
+					text-[15px] leading-none text-mauve11 select-none first:rounded-tl-md last:rounded-tr-md  
+					data-[state=active]:shadow-current data-[state=active]:focus:relative 
+					data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0]
+					 data-[state=active]:focus:shadow-white outline-none cursor-default"
 					value="tab2"
 				>
-					Password
+					Sources
 				</Tabs.Trigger>
 				</Tabs.List>
 				<Tabs.Content
 				className="grow p-5 bg-zinc-900 rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
 				value="tab1"
 				>
-				<p className="mb-5 text-mauve11 text-[15px] leading-normal">
-					Make changes to your account here. Click save when you're done.
-				</p>
-				<fieldset className="mb-[15px] w-full flex flex-col justify-start">
-					<label className="text-[13px] leading-none mb-2.5 text-violet12 block" htmlFor="name">
-					Name
+				<fieldset className="mb-[15px] w-full flex flex-col justify-start pt-4">
+					<label className="text-[13px] leading-none mb-2.5 text-white block" htmlFor="name">
+						Default Download Path
 					</label>
 					<input
-					className="grow shrink-0 rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
-					id="name"
-					defaultValue="Pedro Duarte"
-					/>
-				</fieldset>
-				<fieldset className="mb-[15px] w-full flex flex-col justify-start">
-					<label className="text-[13px] leading-none mb-2.5 text-violet12 block" htmlFor="username">
-					Username
-					</label>
-					<input
-					className="grow shrink-0 rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
-					id="username"
-					defaultValue="@peduarte"
+						className="grow shrink-0 rounded px-2.5 text-[15px] leading-none text-black w-[15rem]
+						shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
+						id="name"
+						defaultValue="Pedro Duarte"
 					/>
 				</fieldset>
 				<div className="flex justify-end mt-5">
-					<button type="button" className="inline-flex items-center justify-center rounded px-[15px] text-[15px] leading-none font-medium h-[35px] bg-green4 text-green11 hover:bg-green5 focus:shadow-[0_0_0_2px] focus:shadow-green7 outline-none cursor-default">
-					Save changes
-					</button>
+					<Button
+						className="float-right bg-zinc-800
+						hover:bg-zinc-700 hover:-translate-y-1 hover:duration-500 transition-all mt-10"
+						onClick={() => setIsDialogOpen(true)}
+					>
+					 Save	
+					</Button>
 				</div>
 				</Tabs.Content>
 				<Tabs.Content
-				className="grow p-5 bg-white rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
+				className="grow p-5 bg-zinc-900 rounded-b-md outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
 				value="tab2"
 				>
-				<p className="mb-5 text-mauve11 text-[15px] leading-normal">
-					Change your password here. After saving, you'll be logged out.
-				</p>
-				<fieldset className="mb-[15px] w-full flex flex-col justify-start">
-					<label
-					className="text-[13px] leading-none mb-2.5 text-violet12 block"
-					htmlFor="currentPassword"
-					>
-					Current password
-					</label>
-					<input
-					className="grow shrink-0 rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
-					id="currentPassword"
-					type="password"
-					/>
-				</fieldset>
-				<fieldset className="mb-[15px] w-full flex flex-col justify-start">
-					<label
-					className="text-[13px] leading-none mb-2.5 text-violet12 block"
-					htmlFor="newPassword"
-					>
-					New password
-					</label>
-					<input
-					className="grow shrink-0 rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
-					id="newPassword"
-					type="password"
-					/>
-				</fieldset>
-				<fieldset className="mb-[15px] w-full flex flex-col justify-start">
-					<label
-					className="text-[13px] leading-none mb-2.5 text-violet12 block"
-					htmlFor="confirmPassword"
-					>
-					Confirm password
-					</label>
-					<input
-					className="grow shrink-0 rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 h-[35px] focus:shadow-[0_0_0_2px] focus:shadow-violet8 outline-none"
-					id="confirmPassword"
-					type="password"
-					/>
-				</fieldset>
-				<div className="flex justify-end mt-5">
-					<button type="button" className="inline-flex items-center justify-center rounded px-[15px] text-[15px] leading-none font-medium h-[35px] bg-green4 text-green11 hover:bg-green5 focus:shadow-[0_0_0_2px] focus:shadow-green7 outline-none cursor-default">
-					Change password
-					</button>
-				</div>
+					<div className="max-h-full border p-5 rounded shadow-zinc-950 shadow-xl">
+						<p className="text-xl">Current Torrent Sources</p>
+						<div className="self-center">
+							<Table className="mt-5 mb-5">
+								<TableHeader className="hover:bg-zinc-900">
+									<TableRow>
+										<TableHead className="w-[160px]">Source Name</TableHead>
+										<TableHead>Status</TableHead>
+										<TableHead className="text-right">Repacks</TableHead>
+									</TableRow>
+								</TableHeader>
+								<TableBody>
+									{/* biome-ignore lint/suspicious/noExplicitAny: <explanation> */}
+									{sources.map((source: any, index) => {
+										return (
+											// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+											<TableRow className="hover:bg-zinc-800/50" key={index}>
+												<TableCell className="font-medium">
+													{String(source.name).slice(1,-1)}
+												</TableCell>
+												<TableCell>Syncronized</TableCell>
+												<TableCell className="text-right">
+													{source.downloadsCount}
+												</TableCell>
+												<TableCell className="text-right">
+													<Button
+														className="bg-zinc-800 hover:bg-red-500
+														hover:-translate-y-1 hover:duration-500 transition-all"
+														onClick={() => removeSourceFromDB(source.name)}
+													>
+														Remove
+													</Button>
+												</TableCell>
+											</TableRow>
+										);
+									})}
+								</TableBody>
+							</Table>
+
+							<Dialog open={isDialogOpen}>
+								<DialogTrigger asChild>
+									<Button
+										className="float-right bg-zinc-800
+										hover:bg-zinc-700 hover:-translate-y-1 hover:duration-500 transition-all mt-10"
+										onClick={() => setIsDialogOpen(true)}
+									>
+										Add
+									</Button>
+								</DialogTrigger>
+								<DialogContent className="sm:max-w-[525px] bg-zinc-950">
+									<DialogHeader>
+										<DialogTitle>Adding Download Sources</DialogTitle>
+										<DialogDescription>
+											To add new sources, set a path to a .json file or a link to a
+											source. Some community-trusted sources can be found here: {""}
+											<a
+												href="https://hydralinks.cloud/sources/"
+												target="_blank"
+												rel="noreferrer"
+											>
+												https://hydralinks.cloud/sources/
+											</a>
+											.
+										</DialogDescription>
+										<div
+											className="absolute right-4 top-2 rounded-sm opacity-70 
+										ring-offset-background hover:bg-zinc-800 
+										hover:-translate-y-[2px] hover:duration-300 
+										transition-all hover:opacity-100 focus:outline-none focus:ring-2 
+										focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none 
+										data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+										>
+											<Cross2Icon
+												className="size-4 cursor-pointer"
+												onClick={() => setIsDialogOpen(false)}
+											/>
+										</div>
+									</DialogHeader>
+									<div className="grid gap-4 py-4">
+										<div className="grid grid-cols-4 items-center gap-4">
+											<Label htmlFor="username" className="text-right">
+												Source Link
+											</Label>
+											<Input
+												id="sourceLink"
+												placeholder="Ex. fitgirl-repacks.site"
+												className="col-span-3"
+												ref={sourceLinkRef}
+											/>
+										</div>
+									</div>
+
+									<DialogFooter>
+										<Button
+											type="submit"
+											onClick={addSourceToDB}
+											className="hover:bg-zinc-800 hover:-translate-y-1
+									hover:duration-500 transition-all"
+										>
+											Save
+										</Button>
+									</DialogFooter>
+								</DialogContent>
+							</Dialog>
+							<Toaster className="bg-slate-950 drop-shadow-xl" />
+						</div>
+					</div>
 				</Tabs.Content>
 			</Tabs.Root>
-			<div className="max-h-full border p-5 rounded shadow-zinc-950 shadow-xl">
-				<p className="text-xl">Current Torrent Sources</p>
-				<div className="self-center">
-					<Table className="mt-5">
-						<TableHeader className="hover:bg-zinc-900">
-							<TableRow>
-								<TableHead className="w-[160px]">Source Name</TableHead>
-								<TableHead>Status</TableHead>
-								<TableHead className="text-right">Repacks</TableHead>
-							</TableRow>
-						</TableHeader>
-						<TableBody>
-							{/* biome-ignore lint/suspicious/noExplicitAny: <explanation> */}
-							{sources.map((source: any, index) => {
-								return (
-									// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-									<TableRow className="hover:bg-zinc-800/50" key={index}>
-										<TableCell className="font-medium">
-											{String(source.name).slice(1,-1)}
-										</TableCell>
-										<TableCell>Syncronized</TableCell>
-										<TableCell className="text-right">
-											{source.downloadsCount}
-										</TableCell>
-										<TableCell className="text-right">
-											<Button
-												className="bg-zinc-800 hover:bg-red-500
-												hover:-translate-y-1 hover:duration-500 transition-all"
-												onClick={() => removeSourceFromDB(source.name)}
-											>
-												Remove
-											</Button>
-										</TableCell>
-									</TableRow>
-								);
-							})}
-						</TableBody>
-					</Table>
-
-					<Dialog open={isDialogOpen}>
-						<DialogTrigger asChild>
-							<Button
-								className="float-right bg-zinc-800 mt-5
-						hover:bg-zinc-700 hover:-translate-y-1 hover:duration-500 transition-all"
-								onClick={() => setIsDialogOpen(true)}
-							>
-								Add
-							</Button>
-						</DialogTrigger>
-						<DialogContent className="sm:max-w-[525px] bg-zinc-950">
-							<DialogHeader>
-								<DialogTitle>Adding Download Sources</DialogTitle>
-								<DialogDescription>
-									To add new sources, set a path to a .json file or a link to a
-									source. Some community-trusted sources can be found here: {""}
-									<a
-										href="https://hydralinks.cloud/sources/"
-										target="_blank"
-										rel="noreferrer"
-									>
-										https://hydralinks.cloud/sources/
-									</a>
-									.
-								</DialogDescription>
-								<div
-									className="absolute right-4 top-2 rounded-sm opacity-70 
-								ring-offset-background hover:bg-zinc-800 
-								hover:-translate-y-[2px] hover:duration-300 
-								transition-all hover:opacity-100 focus:outline-none focus:ring-2 
-								focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none 
-								data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
-								>
-									<Cross2Icon
-										className="size-4 cursor-pointer"
-										onClick={() => setIsDialogOpen(false)}
-									/>
-								</div>
-							</DialogHeader>
-							<div className="grid gap-4 py-4">
-								<div className="grid grid-cols-4 items-center gap-4">
-									<Label htmlFor="username" className="text-right">
-										Source Link
-									</Label>
-									<Input
-										id="sourceLink"
-										placeholder="Ex. fitgirl-repacks.site"
-										className="col-span-3"
-										ref={sourceLinkRef}
-									/>
-								</div>
-							</div>
-
-							<DialogFooter>
-								<Button
-									type="submit"
-									onClick={addSourceToDB}
-									className="hover:bg-zinc-800 hover:-translate-y-1
-							hover:duration-500 transition-all"
-								>
-									Save
-								</Button>
-							</DialogFooter>
-						</DialogContent>
-					</Dialog>
-					<Toaster className="bg-slate-950 drop-shadow-xl" />
-				</div>
-			</div>
 		</div>
 	);
 }
