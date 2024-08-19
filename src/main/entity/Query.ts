d98d8527c1ae27c8077f9be3d87a7232fecec2b5
Patricio0022 <niellucsx@hotmail.com>
2024-08-19 18:13:56 -0300

    json info added in db

--- src/main/entity/Query.ts
+++ src/main/entity/Query.ts
@@ -2,30 +2,6 @@ import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionO
 import path from 'path';
 import fetch from 'node-fetch';
 
-const sqlite3Path = path.join(__dirname,"/src/db/sqlite3.exe");
-const db = new sqlite3.Database(__dirname,'/src/db/cu.db');
-
-const query = `
-CREATE TABLE IF NOT EXISTS json_test (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    downloads TEXT
-);`;
-
-const fetchData = async () => {
-
-    try {
-        consat response = await fetch("https://hydralinks.cloud/sources/fitgirl.json");
-        const jsonData = await response.json();
-
-            jsonData.forEach( (element) => { 
-                const query = `INSERT INTO json_test (name, downloads) VALUES (?,?)  `
-                const array = [element.name, element.downloads] 
-               
-            }); 
-
-    }
-}
 
 
 
