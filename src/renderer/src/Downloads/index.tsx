6c3095cc4e4d4dc90d1ba3f8e6290a3b91dca292
wnccys <ipooo346@gmail.com>
2024-08-19 14:14:49 -0300

    ref: removes unused import

--- src/renderer/src/Downloads/index.tsx
+++ src/renderer/src/Downloads/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import * as React from "react";
-import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, YAxis } from "recharts";
 import {
 	Card,
 	CardContent,
