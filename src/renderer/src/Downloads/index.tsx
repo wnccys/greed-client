9b594056e21df821880c370a3bf7444fb266d3e4
wnccys <ipooo346@gmail.com>
2024-08-19 14:12:21 -0300

    feat: sets correct torrent paused feedback

--- src/renderer/src/Downloads/index.tsx
+++ src/renderer/src/Downloads/index.tsx
@@ -18,12 +18,10 @@ import { Button } from "@renderer/ShadComponents/ui/button";
 import { useEffect, useState } from "react";
 
 type chartData = {
-	downloaded: number,
-	downloadSpeed: number,
-	diskUsage: number,
+	downloaded: number;
+	downloadSpeed: number;
+	diskUsage: number;
 };
-
-// const initialChartData = [{downloaded: 10, downloadSpeed: 240, diskUsage: 300}];
 
 const chartConfig = {
 	downloaded: {
@@ -44,21 +42,24 @@ export function Downloads() {
 		React.useState<keyof typeof chartConfig>("downloadSpeed");
 	const [chartData, setChartData] = useState<chartData[]>([]);
 	const torrentInfo = useDownloads();
+	const [isPaused, setIsPaused] = useState<boolean>();
 
 	const total = React.useMemo(
 		() => ({
 			downloadSpeed: (torrentInfo.downloadSpeed / 8).toFixed(1) || 0,
 			diskUsage: 0,
-		}), [torrentInfo]);
+		}),
+		[torrentInfo],
+	);
 
 	useEffect(() => {
 		const newData: chartData = {
-			downloaded: torrentInfo.downloaded, 
+			downloaded: torrentInfo.downloaded,
 			downloadSpeed: torrentInfo.downloadSpeed,
 			diskUsage: torrentInfo.currentProgress,
 		};
-		
-		setChartData(oldChartData => {
+
+		setChartData((oldChartData) => {
 			const updatedChartData = [...oldChartData, newData];
 			if (updatedChartData.length > 50) {
 				updatedChartData.shift();
@@ -68,6 +69,15 @@ export function Downloads() {
 		});
 	}, [torrentInfo]);
 
+	useEffect(() => {
+		window.electron.ipcRenderer.on(
+			"updateTorrentPauseStatus",
+			(_event, status: boolean) => {
+				setIsPaused(status);
+			},
+		);
+	}, []);
+
 	function DownloadCard({ game }) {
 		if (torrentInfo.currentProgress > 0) {
 			return (
@@ -76,11 +86,11 @@ export function Downloads() {
 					<div className="text-sm pe-5">{game}</div>
 					<div className="w-[45rem]">
 						<div className="pe-4 flex">
-							<div>
-								{torrentInfo.currentProgress}%
-							</div>
+							<div>{torrentInfo.currentProgress}%</div>
 							<div className="flex ps-10 w-full justify-end">
-								<p>{torrentInfo.downloaded} / {torrentInfo.totalSize} MB</p>
+								<p>
+									{torrentInfo.downloaded} / {torrentInfo.totalSize} MB
+								</p>
 							</div>
 						</div>
 						<Progress value={torrentInfo.currentProgress} />
@@ -94,73 +104,73 @@ export function Downloads() {
 
 	return (
 		<div className="flex flex-col container scale-90 content-center h-screen">
-			{ torrentInfo.currentProgress > 0 &&
-			<Card className="bg-black scale-100 pb-10">
-				<CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
-					<div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
-						<CardTitle>
-							Download Stats <span className="text-xs">{"(Mbps)"}</span>
-						</CardTitle>
-						<CardDescription>
-							Downloading: {`${torrentInfo.game}` || "None"}
-						</CardDescription>
-					</div>
-					<div>
-						{["downloadSpeed", "diskUsage"].map((key) => {
-							const chart = key as keyof typeof chartConfig;
-							return (
-								<button
-									type="button"
-									key={chart}
-									data-active={activeChart === chart}
-									className="relative z-30 justify-center border-t px-6 py-4
-								text-left even:border-l data-[active=true]:bg-muted/15
-								sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
-									onClick={() => setActiveChart(chart)}
-								>
-									<div className="flex flex-col">
-										<span className="text-xs text-muted-foreground text-white mb-1">
-											{chartConfig[chart].label}
-										</span>
-										<span className="text-lg font-bold leading-none sm:text-3xl">
-											{total[key as keyof typeof total]}
-										</span>
-									</div>
-								</button>
-							);
-						})}
-					</div>
-				</CardHeader>
-				<CardContent className="px-2 sm:p-6">
-					<ChartContainer
-						config={chartConfig}
-						className="aspect-auto h-[250px] w-full"
-					>
-						<BarChart
-							accessibilityLayer
-							data={chartData}
-							margin={{
-								left: 15,
-								right: 15,
-								top: 40,
-							}}
+			{torrentInfo.currentProgress > 0 && (
+				<Card className="bg-black scale-100 pb-10">
+					<CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
+						<div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
+							<CardTitle>
+								Download Stats <span className="text-xs">{"(Mbps)"}</span>
+							</CardTitle>
+							<CardDescription>
+								Downloading: {`${torrentInfo.game}` || "None"}
+							</CardDescription>
+						</div>
+						<div>
+							{["downloadSpeed", "diskUsage"].map((key) => {
+								const chart = key as keyof typeof chartConfig;
+								return (
+									<button
+										type="button"
+										key={chart}
+										data-active={activeChart === chart}
+										className="relative z-30 justify-center border-t px-6 py-4
+							text-left even:border-l data-[active=true]:bg-muted/15
+							sm:border-l sm:border-t-0 sm:px-8 sm:py-6"
+										onClick={() => setActiveChart(chart)}
+									>
+										<div className="flex flex-col">
+											<span className="text-xs text-muted-foreground text-white mb-1">
+												{chartConfig[chart].label}
+											</span>
+											<span className="text-lg font-bold leading-none sm:text-3xl">
+												{total[key as keyof typeof total]}
+											</span>
+										</div>
+									</button>
+								);
+							})}
+						</div>
+					</CardHeader>
+					<CardContent className="px-2 sm:p-6">
+						<ChartContainer
+							config={chartConfig}
+							className="aspect-auto h-[250px] w-full"
 						>
-							<CartesianGrid vertical={false} />
-							<YAxis capHeight={100} />
-							<Bar dataKey={activeChart} fill={"#ef4444"} />
-						</BarChart>
-					</ChartContainer>
-				</CardContent>
-			</Card>
-			}
+							<BarChart
+								accessibilityLayer
+								data={chartData}
+								margin={{
+									left: 15,
+									right: 15,
+									top: 40,
+								}}
+							>
+								<CartesianGrid vertical={false} />
+								<YAxis capHeight={100} />
+								<Bar dataKey={activeChart} fill={"#ef4444"} />
+							</BarChart>
+						</ChartContainer>
+					</CardContent>
+				</Card>
+			)}
 
 			<div className="flex flex-col gap-4 mt-10">
 				<h1 className="text-2xl">Queue</h1>
 				<div
 					className="size-24 w-full h-full flex flex-col border 
-				border-white bg-zinc-950 rounded-xl gap-2 p-5"
+			border-white bg-zinc-950 rounded-xl gap-2 p-5"
 				>
-					{torrentInfo.currentProgress > 0 && torrentInfo.timeRemaining && (
+					{(torrentInfo.currentProgress >= 0 && torrentInfo.timeRemaining && (
 						<div className="flex h-1/2rem">
 							<DownloadCard game={torrentInfo.game} />
 							<div className="ms-6 flex flex-col w-full">
@@ -169,20 +179,23 @@ export function Downloads() {
 								/>
 								<div className="w-full">
 									<p>{(torrentInfo.downloadSpeed / 8).toFixed(1)} Mbps</p>
-									<ResumePauseTorrent isPaused={torrentInfo.isPaused} 
+									<ResumePauseTorrent
+										isPaused={isPaused}
 										className="mt-1 hover:bg-zinc-800 hover:-translate-y-1
-										hover:duration-500 transition-all" 
+									hover:duration-500 transition-all"
 									/>
-									<Button onClick={() => window.api.removeTorrent()}
+									<Button
+										onClick={() => window.api.removeTorrent()}
 										className="mt-1 hover:bg-red-500 hover:-translate-y-1
-										hover:duration-500 transition-all ms-4"
+									hover:duration-500 transition-all ms-4"
 									>
 										Remove
 									</Button>
 								</div>
 							</div>
 						</div>
-					) || "Nothing Here."}
+					)) ||
+						"Nothing Here."}
 				</div>
 			</div>
 		</div>
@@ -201,25 +214,15 @@ function DownloadTimeRemaining({ timeRemaining }: timeRemainingProps) {
 function ResumePauseTorrent({ isPaused, className }): JSX.Element {
 	if (isPaused) {
 		return (
-			<Button
-				className={className}
-				onClick={() =>
-					window.api.resumeTorrent()
-				}
-			>
-				{ "Resume" }
+			<Button className={className} onClick={() => window.api.resumeTorrent()}>
+				{"Resume"}
 			</Button>
-		)
+		);
 	}
 
 	return (
-		<Button
-			className={className}
-			onClick={() =>
-				window.api.pauseTorrent()		
-			}
-		>
-			{ "Pause" }
+		<Button className={className} onClick={() => window.api.pauseTorrent()}>
+			{"Pause"}
 		</Button>
-	)
+	);
 }
