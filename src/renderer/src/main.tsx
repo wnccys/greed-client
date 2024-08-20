7490a81d35a90ab417924eb78bb1f0fefdb7f54f
Patricio0022 <niellucsx@hotmail.com>
2024-08-20 15:33:24 -0300

    reverse

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,5 +1,5 @@
-import './Assets/main.css'
-import './Assets/global.css'
+import './assets/main.css'
+import './assets/global.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App/App'
