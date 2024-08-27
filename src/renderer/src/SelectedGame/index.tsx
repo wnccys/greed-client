19b75c6d7b6e283441f581c26c83469f51eb1a91
wnccys <ipooo346@gmail.com>
2024-08-27 20:50:00 -0300

    feat: sets validation for invalid images

--- src/renderer/src/SelectedGame/index.tsx
+++ src/renderer/src/SelectedGame/index.tsx
@@ -12,81 +12,89 @@ export function SelectedGame() {
 	useEffect(() => {
 		try {
 			const reader = new FileReader();
-			fetch(`https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/${gameId}/library_hero.jpg`)
-			.then((response) => response.blob())
-			.then((blobImage) => { 
-				reader.onload = () => {
-					setGameImage(reader.result as string);
-					setIsLoading(false);
-				}
-				reader.readAsDataURL(blobImage);
-              });
+			fetch(
+				`https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/${gameId}/library_hero.jpg`,
+			)
+				.then((response) => response.blob())
+				.then((blobImage) => {
+					reader.onload = () => {
+						setGameImage(reader.result as string);
+						setIsLoading(false);
+					};
+					reader.readAsDataURL(blobImage);
+				});
 		} catch (e) {
 			console.log("failed to get game image: ", e);
 		}
 
 		try {
 			const reader = new FileReader();
-			fetch(`https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/${gameId}/logo.png`)
-			.then((response) => response.blob())
-			.then((blobImage) => {
-				reader.onload = () => {
-					setGameIcon(reader.result as string);
-				}
-				reader.readAsDataURL(blobImage);
-			})
-		} catch(e) {
-
-		}
+			fetch(
+				`https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/${gameId}/logo.png`,
+			)
+				.then((response) => response.blob())
+				.then((blobImage) => {
+					reader.onload = () => {
+						setGameIcon(reader.result as string);
+					};
+					reader.readAsDataURL(blobImage);
+				});
+		} catch (e) {}
 	}, [gameId]);
 
-	const selectedGameInfos = window.api.getSelectedGameInfo(gameId);
+	console.log("game image: ", gameImage);
 
-	function SkeletonCard() {
-		return (
-			<div className="flex flex-col space-y-3 z-10">
-				<Skeleton className="h-[25rem] w-full" />
-			</div>
-		)	
-	}
+	const selectedGameInfos = window.api.getSelectedGameInfo(gameId);
 
 	return (
 		<div className="h-screen">
 			<div id="game-cover">
 				<div className="absolute text-lg translate-x-8 translate-y-6 mt-2">
 					<Link to="../catalog">
-						<DoubleArrowLeftIcon className="size-5 delay-150 hover:-translate-y-1
-						 transition hover:scale-105 duration-300 z-20"/>
+						<DoubleArrowLeftIcon
+							className="size-5 delay-150 hover:-translate-y-1
+					 transition hover:scale-105 duration-300 z-20"
+						/>
 					</Link>
 				</div>
-				{ 
-					(gameImage &&
-					(!isLoading && <img src={gameImage} alt="game-cover" />) ||
-					<Skeleton className="h-[25rem] w-full" />) || <div className="bg-black w-full h-[25rem]">cu</div>
-				}
-			</div>
-			
-			<div className="ms-6 absolute -translate-y-[9rem]">
-				{ (gameIcon && <img src={gameIcon} alt="game-icon" className="h-[6rem]" />) || <Skeleton className="h-[25rem]" /> } 
+				<div
+					className={
+						!gameImage?.startsWith("data:text") ? "" : "w-full h-[400px] border"
+					}
+				>
+					{(!isLoading && <img src={gameImage} alt="game-cover" />) || (
+						<Skeleton className="h-[25rem] w-full" />
+					)}
+				</div>
 			</div>
 
-			<div id="play-menu" 
-				className="flex justify-center pt-5 transition delay-150
-				drop-shadow-lg shadow-black duration-300">
+			<div className="ms-6 absolute -translate-y-[9rem]">
+				{(gameIcon && (
+					<img src={gameIcon} alt="game-icon" className="h-[6rem]" />
+				)) || <Skeleton className="h-[25rem]" />}
+			</div>
+
+			<div
+				id="play-menu"
+				className="flex justify-center transition delay-150
+			drop-shadow-lg shadow-black duration-300"
+			>
 				<div
-					className="absolute transform -translate-y-3/4 bg-[#242424] 
+					className="absolute transform -translate-y-1/2 bg-[#242424] 
                         rounded-xl text-white w-[25em] flex justify-center"
 				>
 					<div className="p-3 w-full">
 						<h1>Baldur's Gate III</h1>
-						<p className="text-xs text-zinc-700 font-bold mt-[1em]">Play Time: 1540hrs</p>
+						<p className="text-xs text-zinc-700 font-bold mt-[1em]">
+							Play Time: 1540hrs
+						</p>
 					</div>
 					<div className="h-full self-center p-0">
-						<Button 
-						className="p-6 bg-white text-zinc-900 hover:text-white w-full 
-						h-full ps-10 pe-10 text-lg transition delay-150 duration-300"
-						onClick={() => window.tests.startTorrentDownloadTest()}
-					>
+						<Button
+							className="p-6 bg-white text-zinc-900 hover:text-white w-full 
+					h-full ps-10 pe-10 text-lg transition delay-150 duration-300"
+							onClick={() => window.tests.startTorrentDownloadTest()}
+						>
 							Run
 						</Button>
 					</div>
