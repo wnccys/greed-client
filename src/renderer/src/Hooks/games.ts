3e1b81605c0b92c4637c2467ad381a51876a5047
Patricio0022 <niellucsx@hotmail.com>
2024-08-31 15:09:57 -0300

    merge

--- src/renderer/src/Hooks/games.ts
+++ src/renderer/src/Hooks/games.ts
@@ -19,39 +19,41 @@ export function useCatalogGames(): [Dispatch<SetStateAction<number>>, Game[]] {
 	return selectedGames;
 }
 
-	export function useGamesImages (games: Game[]) {
-    const [images, setImages] = useState<string[]>([]);
+export function useGamesImages(games: Game[], setLoading: (loading: boolean) => void) {
+  const [images, setImages] = useState<string[]>([]);
 
-    useEffect(() => {
-      if (games.length === 0) return; // Early exit if no games
+  useEffect(() => {
+      if (games.length === 0) return;
+
+      setLoading(true);
 
       const fetchImages = async () => {
-        const fetchedImages: string[] = new Array(games.length).fill(''); // Initialize with empty strings
+          const fetchedImages: string[] = new Array(games.length).fill('');
 
-        await Promise.all(games.map(async (game, index) => {
-          try {
-            const response = await fetch(
-              `https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/${game.id}/header.jpg`
-            );
-            const blobImage = await response.blob();
-            const reader = new FileReader();
-            return new Promise<void>((resolve) => {
-              reader.onload = () => {
-                fetchedImages[index] = reader.result as string;
-                resolve();
-              };
-              reader.readAsDataURL(blobImage);
-            });
-          } catch (e) {
-            console.log("Error fetching image: ", e);
-          }
-        }));
+          await Promise.all(
+              games.map(async (game, index) => {
+                  try {
+                      const response = await fetch(
+                          `https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/${game.id}/header.jpg`
+                      );
+                      const blobImage = await response.blob();
+                      const reader = new FileReader();
+                      reader.onload = () => {
+                          fetchedImages[index] = reader.result as string;
+                      };
+                      reader.readAsDataURL(blobImage);
+                  } catch (e) {
+                      console.log("Error fetching image: ", e);
+                  }
+              })
+          );
 
-        setImages(fetchedImages); // Update state once all images are fetched
+          setImages(fetchedImages);
+          setLoading(false); // Set loading to false only after all images are loaded
       };
 
-        fetchImages();
-    }, [games[1]]);
+      fetchImages();
+  }, [games]); // Only depend on 'games' so that it only triggers on page change
 
-    return images;
+  return images;
 }
