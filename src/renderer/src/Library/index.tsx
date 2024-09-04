9844da4f57189aaf0c60dd50c10d88e01f5ba2e3
wnccys <ipooo346@gmail.com>
2024-09-04 10:16:09 -0300

    fixes: corrects library

--- src/renderer/src/Library/index.tsx
+++ src/renderer/src/Library/index.tsx
@@ -11,14 +11,17 @@ import {
 
 export function Library() {
     const library = useLibrary();
+    console.log("library: ", library);
 
-    function DeleteToolTip() {
+    function DeleteToolTip({ gameId }) {
         return (
             <TooltipProvider>
                 <Tooltip>
                     <TooltipTrigger asChild>
                     <Button variant="outline" 
-                    className="hover:bg-red-500 transition-all duration-300 border-none"> 
+                    className="hover:bg-red-500 transition-all duration-300 border-none rounded-none"
+                    onClick={() => window.api.removeGamePath(gameId)}
+                    > 
                         <Cross1Icon color="black" />
                     </Button>
                     </TooltipTrigger>
@@ -31,21 +34,21 @@ export function Library() {
     }
 
     return (
-        <div className="min-h-screen mt-[4rem] flex gap-2 justify-between">
-            {library && (library.map((libraryItem, index) => {
+        <div className="min-h-screen mt-[4rem] flex gap-2 flex-col">
+            {(library.map((libraryItem, index) => {
                 return (
                     // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
                     <div className="ms-10 flex" key={index}>
                         <Card className="ps-4 h-fit flex w-[73vw] items-center border-2
                         bg-white text-black transition-all duration-300 cursor-pointer 
-                        shadow-lg shadow-black hover:shadow-xl hover:shadow-black"
+                        shadow-lg shadow-black hover:shadow-xl hover:shadow-black rounded-none"
                             onClick={() => window.api.execGamePath(libraryItem.execPath)}
                         >
-                            <img src={libraryItem.icon} className="max-h-8 rounded-xl pe-8" alt="game-icon" />
+                            <img src={libraryItem.icon} className="max-h-8 rounded-none pe-8" alt="game-icon" />
                             {libraryItem.gameName}
                         </Card>
                         <div className="self-end flex justify-end gap-2 h-full ms-4">
-                            <DeleteToolTip />
+                            <DeleteToolTip gameId={libraryItem.id} />
                             {/* <Button 
                             className="absolute rounded-sm hover:-translate-y-1
                             transition-all duration-300 bg-zinc-800 hover:bg-red-700 z-50"
