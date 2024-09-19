c4b622985adc5bdd3642888e1506e04bd7874430
wnccys <ipooo346@gmail.com>
2024-09-19 11:30:45 -0300

    tests: sets up correct absolute path resolve

--- tests/greed.test.ts
+++ tests/greed.test.ts
@@ -1,12 +1,13 @@
 import { describe, it } from 'node:test';
 import fs from 'fs';
+import path from 'node:path';
 
 describe("Check for DB file integrity.", async () => {
     it("should be present.", async () => {
-        if (fs.existsSync("./src/db/settings.sqlite")) {
+        if (fs.existsSync(path.resolve("./src/db/settings.sqlite"))) {
             return;
         }; 
-
-        throw Error("Could Not Find DB");
+        console.error("resolved path: ", path.resolve("./src/db/settings.sqlite"));
+        throw Error("Could Not Find Database File at " + path.resolve("./src/db/settings.sqlite"));
     })
 });
