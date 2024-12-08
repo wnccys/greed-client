c990bdb42d2fede54ddf79325616db8044093f0f
wnccys <ipooo346@gmail.com>
2024-12-08 18:53:14 -0300

    feat: removes unused artifacts

--- src/renderer/src/Catalog/GameCard.tsx
+++ src/renderer/src/Catalog/GameCard.tsx
@@ -3,25 +3,23 @@ import { Link } from "react-router-dom";
 
 export function GameCard({ gameName, gameImage, gameId }) {
 	return (
-		<>
-			<Link to={`../selected-game/${gameId}/${gameName}`} className="mt-8">
-				<Card
-					className="border shadow-lg hover:drop-shadow-2xl shadow-black rounded-md max-w-[17.5vw]
-					cursor-pointer transition-all hover:-translate-y-1 hover:scale-105 duration-300 h-fit bg-black
-					relative overflow-hidden"
-				>
-					<CardContent className="p-0">
-						<img src={gameImage} alt="game-cover" className="rounded-lg z-10" />
-						<div
-							className={`text-white absolute -translate-y-[62%] hover:-translate-y-[100%]
-							bg-zinc-950/40 transition-all duration-300 w-[15.6rem] z-20`}
-						>
-							<p className="p-2">{gameName}</p>
-							<p className="text-xs p-3 pt-0 ps-2">{"<GameData>"}</p>
-						</div>
-					</CardContent>
-				</Card>
-			</Link>
-		</>
+		<Link to={`../selected-game/${gameId}/${gameName}`} className="mt-8">
+			<Card
+				className="border shadow-lg hover:drop-shadow-2xl shadow-black rounded-md max-w-[17.5vw]
+				cursor-pointer transition-all hover:-translate-y-1 hover:scale-105 duration-300 h-fit bg-black
+				relative overflow-hidden"
+			>
+				<CardContent className="p-0">
+					<img src={gameImage} alt="game-cover" className="rounded-lg z-10" />
+					<div
+						className={`text-white absolute -translate-y-[62%] hover:-translate-y-[100%]
+						bg-zinc-950/40 transition-all duration-300 w-[15.6rem] z-20`}
+					>
+						<p className="p-2">{gameName}</p>
+						<p className="text-xs p-3 pt-0 ps-2">{"<GameData>"}</p>
+					</div>
+				</CardContent>
+			</Card>
+		</Link>
 	);
 }
