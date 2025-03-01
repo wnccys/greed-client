0e42f9663d9865549be099117b15bd86eb1db726
wnccys <ipooo346@gmail.com>
2025-03-01 12:35:01 -0300

    feat: correct throttling on catalog search

--- src/main/model/gameInfos.ts
+++ src/main/model/gameInfos.ts
@@ -1,6 +1,5 @@
 import { GreedDataSource } from "@main/data-source";
 import { Downloads } from "./entity/Downloads";
-import { throttle } from "lodash-es";
 import { SteamGames } from "./entity/SteamGames";
 import { Like } from "typeorm";
 
@@ -10,14 +9,14 @@ export async function getDBGameInfos(gameId: number) {
 	});
 }
 
-export const getDBGamesByName = throttle(async (name: string) => {
+export const getDBGamesByName = async (name: string) => {
 	return await GreedDataSource.getRepository(SteamGames).find({
 		where: {
 			name: Like(`${name}%`),
 		},
 		take: 20,
 	});
-}, 100);
+};
 
 export const getGamesRange = async (index: number) => {
 	return GreedDataSource.getRepository(SteamGames)
