0fed51cfd4ff085918b5170c5109b697b5a464ec
wnccys <ipooo346@gmail.com>
2025-03-04 23:07:05 -0300

    doc: set summaries

--- src/main/model/gameInfos.ts
+++ src/main/model/gameInfos.ts
@@ -3,12 +3,18 @@ import { Downloads } from "./entity/Downloads";
 import { SteamGames } from "./entity/SteamGames";
 import { Like } from "typeorm";
 
+/**
+ * This function is primarily used for retrieve game's information from Database with a steamId-based approach,
+ */
 export async function getDBGameInfos(gameId: number) {
 	return await GreedDataSource.getRepository(Downloads).findBy({
 		steamId: gameId,
 	});
 }
 
+/**
+ * Return a list of Games fom SteamGames model.
+ */
 export const getDBGamesByName = async (name: string) => {
 	return await GreedDataSource.getRepository(SteamGames).find({
 		where: {
@@ -18,6 +24,10 @@ export const getDBGamesByName = async (name: string) => {
 	});
 };
 
+/**
+ * Get 20 games from SteamGames model starting as index.
+ * Primarily used for catalog pagination;
+ */
 export const getGamesRange = async (index: number) => {
 	return GreedDataSource.getRepository(SteamGames)
 		.createQueryBuilder()
