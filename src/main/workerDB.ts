2bbf05d788c79ab3894e3dd7f05d702c3c21f8af
wnccys <ipooo346@gmail.com>
2025-03-25 19:03:13 -0300

    ref: adjust arg type

--- src/main/workerDB.ts
+++ src/main/workerDB.ts
@@ -1,12 +1,11 @@
 import { parentPort } from "node:worker_threads";
 import { GreedDataSource } from "./data-source";
 import { SteamGames } from "./model/entity/SteamGames";
-import type { SteamJSONGame } from "./model/model";
 
 /** 
  * This worker are responsible for asyncronously set the first-time SteamJSONGames
  * */
-parentPort?.on("message", async (steamGamesArr: SteamJSONGame[]) => {
+parentPort?.on("message", async (steamGamesArr: SteamGames[]) => {
 	GreedDataSource.initialize()
 		.then(() => {
 			GreedDataSource.getRepository(SteamGames).save(steamGamesArr);
