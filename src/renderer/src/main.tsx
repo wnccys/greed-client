e81faf2a32f9d19f04d735f380bc29b80ea061b1
wnccys <ipooo346@gmail.com>
2025-04-25 21:08:00 -0300

    ref: adjust main imports

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,16 +1,16 @@
-import "./Assets/main.css";
-import "./Assets/global.css";
+import "@renderer/Assets/main.css";
+import "@renderer/Assets/global.css";
 import ReactDOM from "react-dom/client";
-import { App } from "./App/App";
+import { App } from "@renderer/App/App";
 import { createHashRouter, RouterProvider } from "react-router-dom";
-import { SelectedGame } from "./SelectedGame";
-import { Catalog } from "./Catalog";
-import { ErrorElement } from "./ErrorElement";
-import { Settings } from "./Settings";
-import { Downloads } from "./Downloads";
-import { Library } from "./Library";
+import { SelectedGame } from "@renderer/SelectedGame";
+import { Catalog } from "@renderer/Catalog";
+import { ErrorElement } from "@renderer/ErrorElement";
+import { Settings } from "@renderer/Settings";
+import { Downloads } from "@renderer/Downloads";
+import { Library } from "@renderer/Library";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./queryClient";
+import { queryClient } from "@renderer/queryClient";
 
 const router = createHashRouter([
 	{
