85b780e8ac5202f3e2a2bb541fd0465505aff61b
wnccys <ipooo346@gmail.com>
2025-12-30 18:09:13 -0300

    ref: remove unused styles

--- src/renderer/src/Catalog/GameCard.tsx
+++ src/renderer/src/Catalog/GameCard.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export function GameCard({ gameName, gameImage, gameId }) {
 	return (
-		<Link to={`../selected-game/${gameId}/${gameName}`} className="mt-8 m-0 p-0 w-fit">
+		<Link to={`../selected-game/${gameId}/${gameName}`} className="mt-8 w-fit">
 			<Card
 				className="border shadow-lg hover:drop-shadow-2xl shadow-black rounded-none max-w-[17.5vw]
 				cursor-pointer transition-all hover:-translate-y-1 hover:scale-105 duration-300 h-fit bg-black
